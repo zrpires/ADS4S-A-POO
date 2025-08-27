02367463cc791c212230db8a33440b30a5d84f10
zrpires <zrpires@github.com>
2025-08-27 20:15:38 -0300

    qualquer coisa

---
+++ src/main/java/com/ZEBOSTA/Minha_morte/MinhaMorteApplication.java
@@ -0,0 +1,13 @@
+package com.ZEBOSTA.Minha_morte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaMorteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhaMorteApplication.class, args);
+	}	
+
+}
