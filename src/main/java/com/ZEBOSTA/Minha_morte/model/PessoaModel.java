e3206a1c3c6fcd5a08cfdd9a6327ba9c758efa2c
José Rafael <130617673+zrpires@users.noreply.github.com>
2025-09-29 21:41:38 -0300

    Commit model

--- src/main/java/com/ZEBOSTA/Minha_morte/model/PessoaModel.java
+++ src/main/java/com/ZEBOSTA/Minha_morte/model/PessoaModel.java
@@ -1,5 +1,28 @@
-package com.ZEBOSTA.Minha_morte.model;
+package com.hc.api.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table
 public class PessoaModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
